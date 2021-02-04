bfc07242b1e9909f2eddb489e614f581a6bbaca2
Nick Rycar <nick@buildkite.com>
2021-02-04 15:53:58 -0800

    Measure once cut twice. Always push to main.

--- rowboat.sh
+++ rowboat.sh
@@ -2,12 +2,33 @@
 
 set -eo pipefail
 
-(( seed_sleep = BUILDKITE_PARALLEL_JOB * 5 ))
+# Really? That's it? That's all I gotta do?
+function inline_image {
+  printf '\033]1338;url='"$1"';alt='"$2"'\a\n'
+}
+
+# Turns out 'sleep 0' is not a good thing to run.
+if [ $BUILDKITE_PARALLEL_JOB == 0 ]; then
+  seed_sleep=1
+else 
+  (( seed_sleep = BUILDKITE_PARALLEL_JOB * 5 ))
+fi
 
 echo "Starting the round..."
 echo "Sleeping $seed_sleep seconds..."
 echo ""
+
+# Seed the Round
 sleep $seed_sleep
+
+# Pick the right GIF for the occasion.
+if [ $BUILDKITE_PARALLEL_JOB == 0 ]; then
+  inline_image 'artifact://artifacts/kirk.gif' "Let's Begin!"
+else
+  inline_image 'artifact://artifacts/kirkandbones.gif' "Row Row Row Your Boat"
+fi
+
+# It's prettier in BASIC, I swear.
 printf "Row"
 sleep 1
 printf ", row"
@@ -32,7 +53,7 @@ printf ", merrily"
 sleep 1
 printf ", merrily"
 sleep 1
-printf ", merrily"
+printf ", merrily..."
 sleep 1
 printf " life"
 sleep 0.5
